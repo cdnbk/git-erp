c10c3cb1fd418f4a7940dc9a906cd937252e3298
cdnbk <cdnbk@qq.com>
2017-08-08 23:01:19 +0800

    添加语言包
    添加语言包

--- js/app.js
+++ js/app.js
@@ -23,7 +23,8 @@ requirejs.config({
         'bootstrap-datetimepicker':'lib/bootstrap-datetimepicker/src/js/bootstrap-datetimepicker',
         'WdatePicker':'lib/My97DatePicker/WdatePicker',
         'zh-cn': 'lib/moment/locale/zh-cn',
-        'linkage':'lib/bootstrap-select/js/bootstrap-select'
+        'linkage': 'lib/bootstrap-select/js/bootstrap-select',
+        'defaults-zh_CN':'lib/bootstrap-select/dist/js/i18n/defaults-zh_CN'
     },
     shim: {
         'WebUploader':{
--- js/controller/linkage/Index.js
+++ js/controller/linkage/Index.js
@@ -1,4 +1,4 @@
-﻿define(['linkage'],
+﻿define([],
     function () {
         $('.selectpicker').selectpicker();
     });
